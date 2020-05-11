90c8858b476fac6e0b9b687c8c309aab069e3b5a
jecueva1997 <reatiguijeferson@gmail.com>
2020-05-11 00:56:37 -0500

    actualizacion

--- Prototipado/src/prototipado/Prototipado.java
+++ Prototipado/src/prototipado/Prototipado.java
@@ -15,7 +15,17 @@ public class Prototipado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] values = {20,50,2,3,1,4,5,6,4,5,6,5,80,9,7,4,5,6,4,5,2,3,1,5,2,4};
+
+        for (int i = 0; i < 10000000; i++) {
+            int index = -1;
+            for (int j = 0; j < values.length; j++) {
+                if (values[j] == 80) {
+                    index = j;
+                    break;
+                }
+            }
+        }
     }
     
 }
