e114fd1cb44db66fc5f4a846144f654e5a431df5
jecueva1997 <reatiguijeferson@gmail.com>
2020-05-11 00:28:02 -0500

    Busqueda Binaria

--- Prototipado/src/BusquedaBinaria.java
+++ Prototipado/src/BusquedaBinaria.java
@@ -3,26 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package prototipado;
 
 /**
  *
  * @author reati
  */
-public class Prototipado {
-
-    /**
-     * @param args the command line arguments
-     */
+public class BusquedaBinaria {
     public static void main(String[] args) {
         int[] values = {20,50,2,3,1,4,5,6,4,5,6,5,80,9,7,4,5,6,4,5,2,3,1,5,2,4};
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 10000000; i++) {
             int index = -1;
             for (int j = 0; j < values.length; j++) {
                 if (values[j] == 80) {
                     index = j;
-                    System.out.println("numero encontrado" + index);
                     break;
                 }
             }
