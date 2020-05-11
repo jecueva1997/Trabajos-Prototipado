aaae224d3f9949276936fe4d7ba4c950742d5caf
jecueva1997 <reatiguijeferson@gmail.com>
2020-05-11 00:20:10 -0500

    Busqueda Binaria

---
+++ poo1/src/funcion/Clase1.java
@@ -0,0 +1,21 @@
+package funcion;
+
+public class Clase1 {
+    public static void main(String[] args) {
+
+
+
+        int[] values = {20,50,2,3,1,4,5,6,4,5,6,5,80,9,7,4,5,6,4,5,2,3,1,5,2,4};
+
+        for (int i = 0; i < 10000000; i++) {
+            int index = -1;
+            for (int j = 0; j < values.length; j++) {
+                if (values[j] == 80) {
+                    index = j;
+                    System.out.println("Elemto encontrado en la posicicion :" +index);
+                    break;
+                }
+            }
+        }
+    }
+}
